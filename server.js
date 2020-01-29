ad46c17b3a2bcb6f97c90d5df720edef5cc6d193
Mattias Lind von Vogelsang <mattelvv@gmail.com>
2020-01-29 09:34:43 +0100

    added comments

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+/***  IMPORTS ***/
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.port || 8000;
+/*** LOCAL MODULE WITH DATABASE OPERTIONS ***/
 const databaseOperations = require("./local_modules/databaseOperations");
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/*** ENDPOINTS ***/
 app.get("/products", async (req, res) => {
   const data = await databaseOperations.getProducts();
   res.send(data);
@@ -32,6 +35,7 @@ app.get("/cart", async (req, res) => {
   res.send(data);
 });
 
+/*** LISTEN TO FIND OUT PORT & INITIATE DATABASE ***/
 app.listen(port, () => {
   console.log("Server started on port ", port);
   databaseOperations.initiateDatabase();
